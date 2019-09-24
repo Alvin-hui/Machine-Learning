4ae143f0b0ecf1f51804baa836a5ed89b06e4bf5
albert_king <764862864@qq.com>
2019-09-24 08:59:50 +0800

    20190924

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -35,7 +35,29 @@ def file2matrix(filename):
         line = line.strip()
         listFromLine = line.split('\t')
         returnMat[index, :] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
+        classLabelVector.append(listFromLine[-1])
         index += 1
     return returnMat, classLabelVector
 
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet/tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLables = file2matrix('/Users/albert_king/Desktop/Machine-Learning/KNN/datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m,:],datingLables[numTestVecs:m],3)
+        print('the classifier came back with: %s, the real answer is: %s' %(classifierResult, datingLables[i]))
+        if(classifierResult != datingLables[i]):errorCount += 1.0
+        print('the total error rate is: %f' %(errorCount/float(numTestVecs)))
