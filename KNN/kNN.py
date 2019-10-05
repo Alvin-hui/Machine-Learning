22f1f07fd521261801cb668c5af415f5a9e461dd
albert_king <764862864@qq.com>
2019-10-05 11:15:08 +0800

    20190924

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import operator
+from os import listdir
 
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -57,7 +58,54 @@ def datingClassTest():
     numTestVecs = int(m*hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m,:],datingLables[numTestVecs:m],3)
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLables[numTestVecs:m], 3)
         print('the classifier came back with: %s, the real answer is: %s' %(classifierResult, datingLables[i]))
         if(classifierResult != datingLables[i]):errorCount += 1.0
         print('the total error rate is: %f' %(errorCount/float(numTestVecs)))
+
+def ckassifyPerson():
+    resultList = ['not at all', 'in small does', 'in large does']
+    percenTats = float(input('percentage of time spent playing video games?'))
+    ffMiles = float(input('frequen flier miles earned per year?'))
+    iceCream = float(input('liters of ice cream consumed per year?'))
+    datingDataMat, datingLables = file2matrix('/Users/albert_king/Desktop/Machine-Learning/KNN/datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percenTats, iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLables, 3)
+    print('you will probably like this person:', resultList[int(classifierResult) - 1])
+
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readlines()
+        for j in range(32):
+            returnVect[0, 32*i + j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLables = []
+    trainingFileList = listdir('/Users/albert_king/Desktop/machinelearninginaction/Ch02/digits/trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLables.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' %fileNameStr)
+    testFileList = listdir('testDigits')
+    err0rCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('_')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' %fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLables, 3)
+        print('the classifier came back with: %d, the real answer is: %d' %(classifierResult, classNumStr))
+        if(classifierResult != classNumStr):
+            err0rCount += 1.0
+    print('\n the total number of error is: %d' %err0rCount)
+    print('\n the total error rate is: %f' %(err0rCount/float(mTest)))
--- test.py
+++ test.py
@@ -10,16 +10,21 @@ from numpy import *
 #print(group)
 #print(lables)
 
-datingDataMat, datingLabels = kNN.file2matrix('/Users/albert_king/Desktop/Machine-Learning/KNN/datingTestSet2.txt')
+#datingDataMat, datingLabels = kNN.file2matrix('/Users/albert_king/Desktop/Machine-Learning/KNN/datingTestSet2.txt')
 
-normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
+#normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
 #print(datingDataMat)
 #fig = plt.figure()
 #ax = fig.add_subplot(111)
 #ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
 #plt.show()
 
-print(normMat)
-print(ranges)
+#print(normMat)
+#print(ranges)
 
-kNN.datingClassTest()
+#kNN.datingClassTest()
+
+#print(kNN.ckassifyPerson())
+#print(kNN.img2vector('/Users/albert_king/Desktop/Machine-Learning/KNN/0_13.txt'))
+
+kNN.handwritingClassTest()
