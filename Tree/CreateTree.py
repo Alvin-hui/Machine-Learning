21828cf8f7eaebd6bf53ace41fe5d3e8052937fc
albert_king <764862864@qq.com>
2019-11-28 23:01:25 +0800

    2019/11/28

--- Tree/CreateTree.py
+++ Tree/CreateTree.py
@@ -1,2 +1,20 @@
+from Tree.ChooseBestFeatureToSplit import chooseBestFeatureToSplit
+from Tree.MajorityCnt import majorityCnt
+from Tree.SplitDataSet import splitDataSet
 def createTree(dataSet, lables):
-    pass
+    classList = [example[-1] for example in  dataSet]
+    if classList.count(classList[0] == len(classList)):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat  = chooseBestFeatureToSplit(dataSet)
+    print(bestFeat)
+    bestFeatLabel = lables[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(lables[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = lables[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
--- Tree/tree.py
+++ Tree/tree.py
@@ -3,12 +3,16 @@ from Tree.CalcShannonEnt import calcshannonent
 from Tree.CreateDataSet import creatDataSet
 from Tree.SplitDataSet import splitDataSet
 from Tree.ChooseBestFeatureToSplit import chooseBestFeatureToSplit
-
+from Tree.CreateTree import createTree
 myDat, lables = creatDataSet()
 
 
 print(myDat)
-print(calcshannonent(myDat))
-print(splitDataSet(myDat, 0, 1))
+classlist = [example[-1] for example in myDat]
+print(classlist)
+#print(lables)
+#print(calcshannonent(myDat))
+#print(splitDataSet(myDat, 0, 1))
 
-print(chooseBestFeatureToSplit(myDat))
+#print(chooseBestFeatureToSplit(myDat))
+#myTree = createTree(myDat, lables)
